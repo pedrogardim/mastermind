7399af33f56a50f3f14c4391b88aead02bc86838
Pedro Gardim <me@pedrogardim.com>
2023-09-28 19:59:51 +0200

    feat: added dynamically color buttons

--- js/templates/game.js
+++ js/templates/game.js
@@ -3,23 +3,21 @@ import { readStorage } from "../utils/localStorage.js";
 
 const difficulties = ["Easy", "Medium", "Hard"];
 
-export const gameTemplate = `
+export const gameTemplate = (numOfColors) => `
     <h1 class="title">MasterMind</h1>
     <div id="game-rows"></div>
     <div class="bottom-container">
         <span id="error-message"></span>
         <div class="color-input-container">
-            <div class="game-color-input"></div>
-            <div class="game-color-input"></div>
-            <div class="game-color-input"></div>
-            <div class="game-color-input"></div>
+            ${Array(numOfColors)
+              .fill('<div class="game-color-input"></div>')
+              .join("")}
             <div class="button" id="check-button">Check</div>
         </div>
         <div class="color-buttons-container">
-            <div class="color-button shadow"></div>
-            <div class="color-button shadow"></div>
-            <div class="color-button shadow"></div>
-            <div class="color-button shadow"></div>
+        ${Array(numOfColors)
+          .fill('<div class="color-button shadow"></div>')
+          .join("")}
         </div>
     </div>
 `;
@@ -27,16 +25,16 @@ export const gameTemplate = `
 export const createGameRow = (index, colors, checkArray) => `
     <div class="shadow game-row">
         <div class="row-counter"><span>${index}</span></div>
-        ${[...colors, ...Array(4 - colors.length)]
+        ${[...colors, ...Array(checkArray.length - colors.length)]
           .map(
             (color) =>
               `<div class="row-color-indicator shadow" style="background-color:${color}"></div>`
           )
-          .reduce((a, b) => a + b)}
+          .join("")}
         <div class="row-checker">
             ${checkArray
               .map((type) => `<div class="row-checker-dot ${type}"></div>`)
-              .reduce((a, b) => a + b)}
+              .join("")}
         </div>
     </div>
 `;
@@ -62,7 +60,7 @@ export const endGameMessage = (message, startDate) => `
             <div>${msToTimeString(time)}s</div>
             `
           )
-          .reduce((a, b) => a + b)}
+          .join("")}
     </div>
     <a class="button" href="./game.html">Start again</a>
 `;
