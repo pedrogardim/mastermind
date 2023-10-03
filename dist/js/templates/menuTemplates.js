13b693201ccde3e5bcec777457f8895b4578a10c
Pedro Gardim <me@pedrogardim.com>
2023-10-03 16:52:42 +0200

    build: first `tsc` run

---
+++ dist/js/templates/menuTemplates.js
@@ -0,0 +1,29 @@
+export const menuTemplate = `
+    <h1 class="title">MasterMind</h1>
+    <span class="label">Your name</span>
+    <input type="text" id="name-input">
+    <span class="label">Difficulty</span>
+    <div class="button-group" id="difficulty-btn-group">
+        <div id="btn-easy">Easy</div>
+        <div id="btn-medium">Medium</div>
+        <div id="btn-hard">Hard</div>
+    </div>
+    <div class="menu-difficulty-info-container">
+        <img class="icon" src="../assets/icons/palette.svg"/>
+        <span id="color-num-indicator"></span>
+        <img class="icon" src="../assets/icons/target.svg"/>
+        <span id="check-num-indicator"></span>
+    </div>
+    <span class="label">Colors</span>
+    <div id="color-inputs-wrapper">
+        <input type="color">
+        <input type="color">
+        <input type="color">
+        <input type="color">
+    </div>
+    <img class="icon clickable" id="reset-colors-button" src="../assets/icons/refresh.svg" >
+    <div>
+        <a href="./about.html" class="button">About</a>
+        <button class="button" id="start-game-button">Start game</button>
+    </div>
+`;
---
+++ dist/js/utils/time.js
@@ -0,0 +1,2 @@
+export const getTimeDifferenceString = (date) => new Date(+new Date() - +date).toTimeString().slice(3, 8);
+export const msToTimeString = (ms) => new Date(ms).toTimeString().slice(3, 8);
