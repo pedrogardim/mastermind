6c7712a3c28d7e1a74e6c2ed9bb0c76515c7608e
Pedro Gardim <me@pedrogardim.com>
2023-10-03 16:49:53 +0200

    chore: types added on games / menu classes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { GameController } from "./game/GameController.js";
 import { GameMenuController } from "./game/GameMenuController.js";
 
+export let gameController: GameController;
+export let gameMenuController: GameMenuController;
 
 document.addEventListener("DOMContentLoaded", () => {
   gameController = new GameController();
